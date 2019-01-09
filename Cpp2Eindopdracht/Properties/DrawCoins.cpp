1979de2582e09dd47f4e6cf6c62316029296905c
ThomThom1988 <tjmvdberg1988@gmail.com>
2019-01-09 11:14:45 +0100

    space

--- Cpp2Eindopdracht/Properties/DrawCoins.cpp
+++ Cpp2Eindopdracht/Properties/DrawCoins.cpp
@@ -3,7 +3,7 @@
 bool DrawCoins::useProperty()
 {
 	card->get_player()->get_player().addGold(amount);
-	card->get_player()->get_socket() << "\r\nJe hebt " << std::to_string(amount) << "goudstukken gekregen.\r\n";
+	card->get_player()->get_socket() << "\r\nJe hebt " << std::to_string(amount) << " goudstukken gekregen.\r\n";
 	if (!game->inCheatMode()) setCanUse(false);
 	return true;
 }
